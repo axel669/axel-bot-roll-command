cea691930fc4dd6318e946ad79448f99e46b5c29
Chris Morgan <haryuu.hanekata@gmail.com>
2021-07-03 21:55:51 -0700

    add helper commands back in

--- command.js
+++ command.js
@@ -1,4 +1,31 @@
 const diceRegex = /((?<sign>\-|\+)?\s*)((?<count>\d+)?d(?<size>\d+)|(?<modifier>\d+))/g
+
+const sum = (total = 0, num) => total + num
+const rollDie = (size) => Math.floor(Math.random() * size) + 1
+const rollDice = (info) => {
+    const {sign = "+", count, size, keep, modifier} = info
+
+    if (modifier !== undefined) {
+        return {
+            sign,
+            rolls: [parseInt(modifier, 10)]
+        }
+    }
+
+    const dieCount = parseInt(count ?? "1", 10)
+    const dieSize = parseInt(size, 10)
+    const rolls = Array
+        .from(
+            {length: dieCount},
+            () => rollDie(dieSize)
+        )
+
+    return {
+        sign,
+        rolls,
+    }
+}
+
 const command = (user, parts) => {
     const username = user["display-name"]
     if (parts.length === 0) {
